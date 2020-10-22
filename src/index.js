c5f4e7e3c7a8ecf5e8a06ec26c165f4239c821bc
Vitali_Vianhura <vengura.code@gmail.com>
2020-10-22 22:50:37 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let str = n.toString();
+    if (str.startsWith("-")) {
+        str = str.substring(1);
+    }
+    return Number(str.split("").reverse().join(""));
 }
